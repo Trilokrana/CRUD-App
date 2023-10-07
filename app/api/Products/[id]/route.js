c76a3f8d0f513b027ce2f4e60137f4832115ca5d
Trilok_rana <ranatrilok1234@gmail.com>
2023-10-07 15:02:32 +0530

    small changes

--- app/api/Products/[id]/route.js
+++ app/api/Products/[id]/route.js
@@ -1,5 +1,5 @@
 import connectToMongoDB from "@/libs/mongoDB";
-import {Product} from "@/models/Products";
+import Product from "@/models/Products";
 import { NextResponse } from "next/server";
 
 export async function PUT(request, { params }) {
