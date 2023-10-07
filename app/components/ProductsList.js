dbf1bbc6147ecedbc44ac14b3e0a1eebfe9e5b6b
Trilok_rana <ranatrilok1234@gmail.com>
2023-10-08 02:23:20 +0530

    impp

--- app/components/ProductsList.js
+++ app/components/ProductsList.js
@@ -18,15 +18,29 @@ const getProducts = async () => {
     }
 }
 
-const ProductsList = async () => {
-    const { products } = await getProducts()
+const ProductsList = () => {
+    const [products, setProducts] = useState(null);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await getProducts();
+            setProducts(response);
+        };
+
+        fetchProducts();
+    }, []);
+
     if (!products) {
+        return <div>Loading...</div>;
+    }
+
+    if (products.length === 0) {
         return <div>Error fetching products</div>;
     }
 
     return (
         <>
-            {Products.map((product) => (
+            {products.map((product) => (
                 <div key={product._id} className='p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start rounded-lg hover:border-slate-500 hover:bg-gray-100 transition-all' >
                     <div>
                         <h1 className='font-bold text-2xl'>Product Name: {product.title}</h1>
