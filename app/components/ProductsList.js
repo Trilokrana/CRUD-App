e22a8d4957d30181f412d6e9844e5b12fb5c2410
Trilok Rana <58104039+Trilokrana@users.noreply.github.com>
2023-10-07 23:18:13 +0530

    Update ProductsList.js

--- app/components/ProductsList.js
+++ app/components/ProductsList.js
@@ -17,13 +17,13 @@ const getProducts = async () => {
 }
 
 const ProductsList = async () => {
-    const { products } = await getProducts()
-    if (!products) {
+    const { Products } = await getProducts()
+    if (!Products) {
         return <div>Error fetching products</div>;
     }
     return (
         <>
-            {products.map((product) => (
+            {Products.map((product) => (
                 <div key={product._id} className='p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start rounded-lg hover:border-slate-500 hover:bg-gray-100 transition-all' >
                     <div>
                         <h1 className='font-bold text-2xl'>Product Name: {product.title}</h1>
